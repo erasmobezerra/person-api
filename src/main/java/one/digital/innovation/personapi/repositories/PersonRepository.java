272df301482d5f23abf38278a094c53f86b3de25
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2021-09-03 14:40:23 -0300

    clean project

--- src/main/java/one/digital/innovation/personapi/repositories/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package one.digital.innovation.personapi.repositories;
-
-import one.digital.innovation.personapi.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
