7e18adb68f3fb1dfaf2147269714e7c13abff4ff
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2021-09-03 14:09:50 -0300

    controller

--- src/main/java/one/digital/innovation/personapi/controller/PersonController.java
+++ src/main/java/one/digital/innovation/personapi/controller/PersonController.java
@@ -1,14 +1,16 @@
 package one.digital.innovation.personapi.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/people")
+@RequestMapping("/person")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
-
-    @GetMapping
+    @GetMapping("/person")
     public String getBook(){
         return "API Rest";
     }
